3f6cbfa048797da2fb4639a9015f03b347ea0cd7
dfldiego <dfl.diego92@gmail.com>
2020-07-30 21:38:23 -0300

    modificando html de los componentes con bootstrap, rutas, estilos generales del sitio

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PlatosComponent } from './components/platos/platos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'platos', component: PlatosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' } //si agrega cualquier cosa en la ruta redireccionar a "home"
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
