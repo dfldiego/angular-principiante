0e36dc5422b85cf02580e91fbacbe2a83d0af202
dfldiego <dfl.diego92@gmail.com>
2020-08-03 15:58:07 -0300

    imagenes agregadas, error para leer json

--- angular-frontend/src/app/components/platos/platos.component.ts
+++ angular-frontend/src/app/components/platos/platos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeliveryService } from 'src/app/services/delivery.service';
 
 @Component({
   selector: 'app-platos',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servicioDelivery: DeliveryService) { }
 
   ngOnInit(): void {
   }
