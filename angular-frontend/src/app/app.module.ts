dc305ab1a394a5af799bbf71928dfcb6f489ee2b
dfldiego <dfl.diego92@gmail.com>
2020-08-10 12:50:36 -0300

    Angular HTTP. Metodos HTTP en Services. Agregando libreria http al proyecto.

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { ItemPlatoComponent } from './components/item-plato/item-plato.component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule
   ],
--- angular-frontend/src/app/services/delivery.service.ts
+++ angular-frontend/src/app/services/delivery.service.ts
@@ -1,44 +1,76 @@
 import { Injectable } from '@angular/core';
 import * as data from 'src/assets/datos/platos.json';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Plato } from '../entities/Plato';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeliveryService {
 
-  platosFile: any = (data as any).default;
+  //platosFile: any = (data as any).default;
+  public platosData: Plato[] = [];
+  public platoEncontrado: Plato;
 
-  constructor() {
+  constructor(public http: HttpClient) {
     console.log("Servicio Cargado!!!");
-    console.log(this.platosFile);
   }
 
   public getPlatos(): any[] {
-    return this.platosFile.platos;
-    console.log(this.platosFile.platos);
+    return this.platosData;
+    console.log(this.platosData);
   }
 
   public getPlatoXId(idx: string): any {
-    for (let plato of this.platosFile.platos) {
+    for (let plato of this.platosData) {
       if (plato.id == idx) {
         return plato;
       }
     }
   }
 
-  public buscarPlatos(termino: string): any[] {
-    let platosArr: any[] = [];
-    termino = termino.toLowerCase();
+  //lee todos los platos  
+  getPlatosFromDataBase() {
+    return this.http.get("http://localhost:8081/WebAppServer/RestoServlet?action=listar").pipe(map(platosData => platosData));
+  }
 
-    for (let plato of this.platosFile.platos) {
-      let nombre = plato.nombre.toLowerCase();
-      if (nombre.indexOf(termino) >= 0) {
-        platosArr.push(plato);      //me puede retornar un array de platos.
-      }
+  //busca un plato por el id
+  getPlatoEnBaseDatosXId(idx: string) {
+    return this.http.get("http://localhost:8081/WebAppServer/RestoServlet?action=buscar&idPlato=" + idx).pipe(map(platoEncontrado => platoEncontrado));
+  }
 
-    }
-    return platosArr;
+  //busca los platos por un terminode busqueda
+  getPlatosBusquedaFromDataBase(termino: string) {
+    return this.http.get("http://localhost:8081/WebAppServer/RestoServlet?action=busqueda&termino=" + termino).pipe(map(platosSearch => platosSearch));
+  }
+
+  platoAdminUrl: string = "http://localhost:8081/WebAppServer/RestoServlet";
+  newPlato(platoNuevo: Plato) {
+    return this.http.post<Plato>(this.platoAdminUrl, null, {
+      params: new HttpParams().set("action", "insertar").set("id", "0").set("nombre", platoNuevo.nombre).set("imagenPath", platoNuevo.imagenPath).set("precio", platoNuevo.precio).set("rubro", platoNuevo.rubro)
+    }).pipe(map(nuevoPlato => {
+      console.log(nuevoPlato.nombre);
+      return nuevoPlato;
+    }));
+  }
+
+  updatePlato(platoUpdate: Plato) {
+    return this.http.post<Plato>(this.platoAdminUrl, null, {
+      params: new HttpParams().set("action", "actualizar").set("id", platoUpdate.id)
+        .set("nombre", platoUpdate.nombre).set("imagenPath", platoUpdate.imagenPath).set("precio", platoUpdate.precio).set("rubro", platoUpdate.rubro)
+    }).pipe(map(res => {
+      console.log(res.nombre);
+      return res;
+    }));
+  }
+
+  deletePlato(idPlato: string) {
+    return this.http.post(this.platoAdminUrl, null, { params: new HttpParams().set("action", "eliminar").set("id", idPlato) })
+      .pipe(
+        map(res => {
+          console.log(res);
+          return res;
+        }));
   }
 }
-
-
